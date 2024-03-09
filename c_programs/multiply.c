0aab58c372d2f5c3169dd5d875706d7b8478c29f
lakshya-sharmaa <lakshyas7999@gmail.com>
2024-03-09 21:01:38 +0530

    update

---
+++ c_programs/multiply.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b;
+    printf("Enter the 1st Number: ");
+    scanf("%i", &a);
+    printf("Enter the 2nd Number: ");
+    scanf("%i", &b);
+
+    printf("Multiplication: %i\n", a * b);
+}
