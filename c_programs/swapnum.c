f0f21ee91f4310e290865914c5f857a06bbf9bd9
lakshya-sharmaa <lakshyas7999@gmail.com>
2024-03-09 20:52:06 +0530

    update

---
+++ c_programs/swapnum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 2, b = 4;
+    printf("1st Number is: %i\n", a);
+    printf("2nd Number is: %i\n", b);
+    int temp = a;
+    a = b;
+    b = temp;
+    printf("1st number is: %i\n", a);
+    printf("1st number is: %i\n", b);
+}
