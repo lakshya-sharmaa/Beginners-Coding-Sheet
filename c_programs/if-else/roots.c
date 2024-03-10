b977ebd89f6666c8c3796b4422c9718e4b0f9e94
lakshya-sharmaa <lakshyas7999@gmail.com>
2024-03-10 13:59:00 +0530

    upadate

---
+++ c_programs/if-else/roots.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    float a, b, c;
+
+    printf("Enter the Number: ");
+    scanf("%f", &a);
+    printf("Enter the Number: ");
+    scanf("%f", &b);
+    printf("Enter the Number: ");
+    scanf("%f", &c);
+
+    float dis = b * b - 4 * a * c;
+
+    if (dis > 0)
+    {
+        float root1 = (-b + sqrt(dis)) / (2 * a);
+        float root2 = (-b - sqrt(dis)) / (2 * a);
+        printf("%f and %f\n", root1, root2);
+    }
+    else if (dis == 0)
+    {
+        float root = -b / (2 * a);
+        printf("%f\n", root);
+
+    }
+    else
+    {
+        float realpart = -b / (2 * a);
+        float imaginarypart = sqrt(dis / (2 * a));
+
+        printf("real part: %f and imagainary %f\n", realpart, imaginarypart);
+    }
+}
