a02023af512dcc51d8131bbae6c79c5837e6a336
lakshya-sharmaa <lakshyas7999@gmail.com>
2024-03-11 18:24:02 +0530

    update

---
+++ c_programs/loops/sumall.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Write a Program to display sum of all digits of a given
+// Number N by user
+
+int main(void)
+{
+    int num;
+    printf("Enter The Number");
+    scanf("%i", num);
+    
+}
