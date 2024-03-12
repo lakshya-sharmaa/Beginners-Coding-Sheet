db22c3a9eb45f3b9ffb9fa6c4cbd1fdc85a072ed
lakshya-sharmaa <lakshyas7999@gmail.com>
2024-03-12 10:10:00 +0530

    update

--- c_programs/patterns/pattern9.c
+++ c_programs/patterns/pattern9.c
@@ -8,14 +8,14 @@ int main(void)
     printf("Enter the Number: ");
     scanf("%i", &n);
 
-    for (int i=0; i<=n; i++)
+    for (int i=0; i < n; i++)
     {
         int coefficient = 1;
-        for (int j=0; j<=n-i-1; j++)
+        for (int j=0; j < n-i-1; j++)
         {
             printf(" ");
         }
-        for (int k=0; k<=i; k++)
+        for (int k=0; k <= i; k++)
         {
             printf("%i   ", coefficient);
             coefficient = coefficient * (i - k) / (k + 1);
