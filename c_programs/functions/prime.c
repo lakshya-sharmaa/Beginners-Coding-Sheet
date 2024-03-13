8be9e30f1314129e0977866703c8829c1d6718da
lakshya-sharmaa <lakshyas7999@gmail.com>
2024-03-13 10:59:36 +0530

    update

---
+++ c_programs/functions/prime.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+// Write a Program to Display Prime Numbers Between Two
+// Intervals (entered by the user) Using Functions
+
+int prime(int num);
+void primenum(int a, int b);
+
+int main(void)
+{
+    int num1, num2;
+    do
+    {
+    printf("Enter Number: ");
+    scanf("%i", &num1);
+    if(num1 <= 1)
+    {
+        printf("Enter the valid Number\n");
+    }
+    }while (num1 <= 1);
+    do
+    {
+    printf("Enter The Number: ");
+    scanf("%i", &num2);
+
+    if(num2 <= num1)
+    {
+        printf("Enter the number greater than %i\n", num1);
+    }
+
+    }while (num2 <= num1 || num2 <= 1);
+
+    primenum(num1, num2);
+}
+
+
+int prime(int num)
+{
+    if(num <= 1)
+    {
+       return 0;
+    }
+
+    for(int i=2; i * i <= num; i++)
+    {
+        if (num % i == 0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void primenum(int a, int b)
+{
+    for (int i=a; i<=b; i++)
+    {
+        if(prime(i))
+        {
+            printf("Prime Numbers are: %i\n", i );
+        }
+        // printf("\n");
+    }
+}
